7ec006e369a416855707f307d7212e1b9f90fc2b
Ivan Mikhailov <meehighlov@MacBook-Air.local>
2024-08-09 15:15:56 +0300

    fix

--- internal/db/entities.go
+++ internal/db/entities.go
@@ -103,7 +103,7 @@ func (e *Event) Validated() (*Event, error) {
 }
 
 func (e *Event) UpdateNotifyAt() (string, error) {
-	notifyAt, err := time.Parse("02.01.2006 15:04:05", e.NotifyAt)
+	notifyAt, err := time.Parse("02.01.2006 15:04", e.NotifyAt)
 	if err != nil {
 		return "", err
 	}
